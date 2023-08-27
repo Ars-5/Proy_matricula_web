029581510ebe9ac9502ec02435bee6ce01283a45
Ars-5 <manticora1999@gmail.com>
2023-08-27 03:26:14 -0500

    v1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+import { ExamenComponent } from './examen/examen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConsultaComponent,
+    ExamenComponent
   ],
   imports: [
     BrowserModule
